78f893a2e49051e54675aae18a3484e8b3eccd02
christopher <christopherstefano1@gmail.com>
2021-04-21 10:24:42 -0300

    pronto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>GUIA DE COISAS </h1>
+    <clientes/>
+    <produto/>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import clientes from "./components/clientes";
+import Produto from './components/produto.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    clientes,
+    Produto
   }
 }
 </script>
+   
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/produto.vue
@@ -0,0 +1,16 @@
+<template >
+    <div>
+    <h4>produto</h4>
+    <input type="text" placeholder="preço ">
+    
+    </div>
+
+</template>        
+<script>
+export default {
+    
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
